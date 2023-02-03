f365a8503f4f8053bd767cb9644364af8abffb39
Josue Alvarado Anaya <luisjo.lvrd@gmail.com>
2023-02-03 11:51:02 -0600

    Cambios en funcion textoValido.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function textoValido(){
     let texto = document.querySelector(".areatexto").value;
     let validar = texto.match(/^[a-z]*$/);
 
-    if (!validar || validar == 0 ){
+    if (validar == 0 ){
         alert("Favor de ingresar letras en minúsculas y sin acentos.");
         ocultar();
         document.querySelector(".areatexto").value = "";
